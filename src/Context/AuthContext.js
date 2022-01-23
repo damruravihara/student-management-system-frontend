7cba9a87e4531f241a33d1f2a38168d647bfa6d8
DamruRavihara <damruravihara2@gmail.com>
2022-01-23 19:52:36 +0530

    test commit 2

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -18,9 +18,14 @@ export default ({ children })=>{
   },[]);
   return(
     <div>
+      {!isLoaded ? <div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+   
+  </div>
+</div>:
       <AuthContext.Provider value={{user,setUser,isAuthenticated,setIsAuthenticated}}>
         { children}
-        </AuthContext.Provider>
+        </AuthContext.Provider>}
     </div>
   )
 }
